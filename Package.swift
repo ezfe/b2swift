abf0d3a0303631f1c7bd1229b1e6e636e77caba7
Ezekiel Elin <ezekielelin@me.com>
2018-06-15 14:24:03 -0400

    Removed dependency again cause I didn't do it properly before

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "b2swift",
-            dependencies: ["SwiftyJSON", "Files", "CryptoSwift", "Async"]),
+            dependencies: ["Files", "CryptoSwift", "Async"]),
         .testTarget(
             name: "b2swiftTests",
             dependencies: ["b2swift"]),
